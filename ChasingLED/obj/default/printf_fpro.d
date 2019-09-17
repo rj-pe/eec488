4f5387af293ecd680e7eab8bf3ccac2be157c39c
rj <rj@ratjuice.windchill.net>
2019-09-17 13:09:00 -0400

    removed old project files for lab2

--- ChasingLED/obj/default/printf_fpro.d
+++
@@ -1,8 +0,0 @@
-obj/default/printf_fpro.o: printf_fpro.c chu_io_rw.h chu_io_map.h \
- printf.h
-
-chu_io_rw.h:
-
-chu_io_map.h:
-
-printf.h:
--- ChasingLED/obj/default/uart_core.d
+++
@@ -1,8 +0,0 @@
-obj/default/uart_core.o: uart_core.cpp uart_core.h chu_io_rw.h \
- chu_io_map.h
-
-uart_core.h:
-
-chu_io_rw.h:
-
-chu_io_map.h:
