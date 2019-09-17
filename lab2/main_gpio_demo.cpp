19661460d57bb8d6f6318d572a318225ad3a7ab7
r.j <rjpe@protonmail.com>
2019-09-17 13:48:17 -0400

    Update main_gpio_demo.cpp

--- lab2/main_gpio_demo.cpp
+++ lab2/main_gpio_demo.cpp
@@ -14,43 +14,43 @@
 #define elapsed_ms(time_stamp) (now_ms()-(time_stamp))
 
 int main() {
-	uint16_t switches = 0;                // Storage for the board's switch state.
-	int n = 0;                            // Led index counter for led array.
-	int leftDirection = 1;                // Keep track of direction of sequence.
-	int ts0;                              // Timestamp when last led switched.
-	int ledTime;                          // Frequency of the displayed sequence.
+	uint16_t switches = 0;                      // Storage for the board's switch state.
+	int n = 0;                                  // Led index counter for led array.
+	int leftDirection = 1;                      // Keep track of direction of sequence.
+	int ts0;                                    // Timestamp when last led switched.
+	int ledTime;                                // Frequency of the displayed sequence.
 	while (1) {
 		
 		switches = sw.read();               // read ten switches 
 		ledTime = (switches >> 4) * 10;     // record sequence frequency from switch
 		if (!(switches & 0x0002)){          // Ensure that pause switch is off      
-                                        // and sequence should move to the left.
-                                        // Check if it's time to advance led.
+                                                    // and sequence should move to the left.
+                                                    // Check if it's time to advance led.
 			if((elapsed_ms(ts0) > (ledTime)) && leftDirection) {
-			ts0 = now_ms();                   // Record the current time.
-			led.write(0x001 << n);            // Turn on only the adjacent led.
-			n++;                              // Increment the led index counter.
-			if(n > 9) {                       // Pattern is at end of led array.
-				leftDirection = 0;              // Change sequence direction to right. 
-				n = 8;                          // Hard reset of led index counter.
+			ts0 = now_ms();             // Record the current time.
+			led.write(0x001 << n);      // Turn on only the adjacent led.
+			n++;                        // Increment the led index counter.
+			if(n > 9) {                 // Pattern is at end of led array.
+				leftDirection = 0;  // Change sequence direction to right. 
+				n = 8;              // Hard reset of led index counter.
 			}
 			}
-                                        // While moving to the right
-                                        // repeat same procedure as above.
+                                                    // While moving to the right
+                                                    // repeat same procedure as above.
 			else if((elapsed_ms(ts0) > (ledTime)) && !leftDirection) {
 			ts0 = now_ms();
 			led.write(0x001 << n);
 			n--;
-			if(n < 0) {                       // pattern at start of led array
-				leftDirection = 1;              // change sequence to left
-				n = 1;                          // hard reset of led index counter
+			if(n < 0) {                 // pattern at start of led array
+				leftDirection = 1;  // change sequence to left
+				n = 1;              // hard reset of led index counter
 			}
 			}
 		}
 		if (switches & 0x0001){             // if the initialize switch is up
-			n = 0;                            // hard reset of led index counter
-			leftDirection = 1;                // initial sequence direction to left
-			led.write(0x001 << n);            // turn on only the righmost led
+			n = 0;                      // hard reset of led index counter
+			leftDirection = 1;          // initial sequence direction to left
+			led.write(0x001 << n);      // turn on only the righmost led
 		}
 	}
 }
