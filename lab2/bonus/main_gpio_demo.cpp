c69e6b36f3c80ca756ed0d2f64a12c32a0e37656
R.J <rjpe@protonmail.com>
2019-09-18 13:17:25 -0400

    Update main_gpio_demo.cpp

--- lab2/bonus/main_gpio_demo.cpp
+++ lab2/bonus/main_gpio_demo.cpp
@@ -1,18 +1,16 @@
-//#include "de10_baseline.h"
-//int main() {
-//   uint32_t pattern;
-//   while (1) {
-//      // read switch bit 0
-//      int turn_on = sw.read(0);
-//      // set led pattern
-//      if (turn_on)
-//         pattern = 0x00003ff;
-//      else
-//         pattern = 0x0000000;
-//      // write led pattern
-//      led.write(pattern);
-//   }
-//}
+/**********************************************************************
+ file: chasing_leds_bonus.cpp - turns two seperate led's on and off sequentially
+ 
+ An array of ten led's is controlled by switches.
+ One led sequence lights sequentially starting from the right side moving to the left.
+ The other led sequence lights sequentially starting from the left side moving to the right.
+ After the leftmost led turns on, the sequence changes directions to the right.
+ The six leftmost switches control the speed of the pattern. The switches
+ represent a binary number n, and the speed of the pattern is n * 10 ms.
+ Flipping switch 0 up initializes the process by turning on the rightmost led, 
+ when switch 0 is flipped back down the chase begins. Switch 1 pauses the chase.
+ 
+ **********************************************************************/
 #include "de10_baseline.h"
 #define elapsed_ms(time_stamp) (now_ms()-(time_stamp))
 
