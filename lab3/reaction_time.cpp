b21b10c7d09bec716d9c001d5b1239f5fa23b4a6
matthew meadwell <mmeadwell22@gmail.com>
2019-09-20 15:48:24 -0400

    initial upload of main for lab 3

---
+++ lab3/reaction_time.cpp
@@ -0,0 +1,13 @@
+/*
+ * Michael: turn on count, slow, react
+ * RJ: ready, wait, cheat
+ * Matt: main, init
+ *
+ * Bonus: be able to store top three reaction times keep this in mind while developing.
+ * */
+
+#include "de10_baseline.h"
+
+int main(){
+
+}
