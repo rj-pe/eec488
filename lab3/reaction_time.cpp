0919958c93800aaa2db0ebca89ee2eaeedcb99e3
rj <rj@ratjuice.windchill.net>
2019-09-22 20:59:39 -0400

    added new states

--- lab3/reaction_time.cpp
+++ lab3/reaction_time.cpp
@@ -19,10 +19,31 @@
  * Led0 is off.
  * @param &state The address of the current state variable.
  */
-int ready(uint32_t* state_pntr){
+void ready(uint32_t* state_pntr){
   sseg.write_8ptn(CLR_PTN);
   led.write(1, 0);
-  *state = 1;
+  *state_pntr = 1;
+}
+
+/**
+ * Sets the board to wait state.
+ * Seven segment displays zeros.
+ * Led0 is off.
+ * @param &state The address of the current state variable.
+ */
+void wait(uint32_t* state_pntr){
+  sseg.write_8ptn(ZERO_PTN);
+  // TODO figure out how to set decimal point
+  led.write(1, 0);
+  *state_pntr = 2;
+}
+
+/**
+ * Sets the board to cheat state.
+ * @param &state The address of the current state variable.
+ */
+void cheat(uint32_t* state_pntr){
+  *state_pntr = 4;
 }
 
 
