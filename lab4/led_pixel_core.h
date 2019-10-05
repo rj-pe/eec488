e63ae4c2bb81c17e59f409bbdcb4712da7fac25d
r p <rjpe@protonmail.com>
2019-10-05 19:20:55 -0400

    added comments to header file

--- lab4/led_pixel_core.h
+++ lab4/led_pixel_core.h
@@ -1,3 +1,7 @@
+/**
+ * LedPixelCore.h
+ */
+
 #ifndef LED_PIXEL_CORE_H_
 #define LED_PIXEL_CORE_H_
 
@@ -15,24 +19,50 @@ public:
       RESOLUTION_BITS = 10,
       MAX = 1 << RESOLUTION_BITS
    };
+  /**
+  * Constructor for the L.E.D. pixel class.
+  * @param core_base_addr base address of PWM instance
+  * @param r_ch PWM channel controlling the red LED
+  * @param g_ch PWM channel controlling the green LED
+  * @param b_ch PWM channel controlling the blue LED
+  */
    LedPixelCore(uint32_t core_base_addr, int r_ch, int g_ch, int b_ch);
    ~LedPixelCore();
+   /**
+   * Set frequency of the L.E.D.
+   * @param freq the frequency at which to set the L.E.D.
+   */
    void set_freq(int freq);
+   /**
+   * Specify whether the tricolor LED is active-high or active-low
+   * @note our lab's tricolor LED is common anode (active low)
+   * @param level 0 for active-low; and non-zero for active high
+   */   
    void set_active_level(int level);
+   /**
+   * Set the pixel color.
+   * @param r 8-bit unsigned number indicating the intensity of red (0-255)
+   * @param g 8-bit unsigned number indicating the intensity of green (0-255)
+   * @param b 8-bit unsigned number indicating the intensity of blue (0-255)
+   */   
    void set_color(uint8_t r, uint8_t g, uint8_t b);
+   /**
+   * Set brightness of the tricolor LED pixel between 0.0 & 1.0
+   * @param br brightness of the pixel (0.0-1.0) 1.0 is the brightest.
+   */   
    void set_brightness(float br);
 private:
-   uint32_t base_addr;
-   uint32_t freq;
-   uint8_t r_pwm;
-   uint8_t g_pwm;
-   uint8_t b_pwm;
-   uint8_t r_clr;
-   uint8_t g_clr;
-   uint8_t b_clr;
-   float brightness;
-   int active;
+   uint32_t base_addr; ///< The base address of the PWM core
+   uint32_t freq;      ///< The frequency of the LED 
+   uint8_t r_pwm;      ///< The red channel register
+   uint8_t g_pwm;      ///< The green channel register
+   uint8_t b_pwm;      ///< The blue channel register
+   uint8_t r_clr;      ///< The red color level
+   uint8_t g_clr;      ///< The green color level
+   uint8_t b_clr;      ///< The blue color level
+   float brightness;   ///< The brightness of the LED
+   int active;         ///< Whether the LED is CC or CA
 };
 
 
-#endif
+#endif LEDPIXELCORE_H_
