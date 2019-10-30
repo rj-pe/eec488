8b11a347cb01fbd786e7bba760549f9c7021c04b
rj <rjpe@ieee.org>
2019-10-30 09:19:22 -0400

    added chirp vhd code

---
+++ lab7/chirp_gen_top_test/chirp_gen_top_test.vhd
@@ -0,0 +1,60 @@
+-- file chirp_gen_top_test.vhd
+-- testing circuit for top level design of ping generator
+
+ library ieee;
+ use ieee.std_logic_1164.all;
+ use ieee.numeric_std.all;
+
+entity chirp_gen_top_test is
+  port(
+    clk : in std_logic;
+    sw: in std_logic_vector(0 downto 0);
+    led : out std_logic_vector(0 downto 0)
+	 -- arduino_io : out std_logic_vector(7 downto 7) 
+  );
+end chirp_gen_top_test;
+
+architecture arch of chirp_gen_top_test is
+  constant PRD: unsigned := "100011110000110100011000000"; -- 1.5 s
+  constant M : integer := 17000;
+  signal pulse_s, gate_s, trig_s: std_logic;
+  signal count_s: std_logic_vector(29 downto 0);
+
+begin
+  trig_s <= sw(0);
+  led(0) <= (gate_s and pulse_s);
+  -- arduino_io(7) <= (gate_s and pulse_s);
+  -- finite state machine produces gate signal
+  fmsd_unit: entity work.fsmd
+    port map(
+      clk =>  clk,
+      trig => trig_s,
+      dvsr => PRD,
+      gate => gate_s
+    );
+  -- counter unit for chirp 
+  counter_unit: entity work.counter
+    port map(
+      clk =>     clk,
+      count_o => count_s
+    );
+  -- ddfs unit
+  ddfs_unit: entity work.ddfs
+    port map(
+      clk =>       clk,
+      reset =>     '0',
+      fccw =>      count_s, -- gate input
+      focw =>      (others => '0'),
+      pha =>       (others => '0'),
+      env =>       (others => '0'),
+      pcm_out =>   open, 
+      pulse_out => pulse_s
+      --fccw => std_logic_vector(to_unsigned(M, 30)), -- gate input
+      --focw => std_logic_vector(to_unsigned(count_s, 30)), -- counter input
+      --pha => (others => '0'),
+      --env => std_logic_vector(to_unsigned(1, 30)), -- must be non-zero
+      --pcm_out => pulse_s, -- output is a sine wave with varying freq
+      --pulse_out => open
+    );
+
+end arch;
--- lab7/cpp/ping_gen_quick_main.cpp
+++ lab7/cpp/ping_gen_quick_main.cpp
@@ -6,36 +6,25 @@
 
 int main(){
    /***********************************************************
-    *  the quick and "dirty" check of a custom I/O core
+    ** a quick and "dirty" check of a ping generator IP core **
     ***********************************************************/
    // get the base address of the custom core
    // the blink core is attached to slot 4 (for user core)
-   int blink_base = get_slot_addr(BRIDGE_BASE, 4);
+   int ping_base = get_slot_addr(BRIDGE_BASE, 4);
    while(1){
-      // fccw -> register 0, bits 29 - 0
-      // dvsr -> register 1, bits 26 - 0
-      // trig -> register 2, bit 0
-      // set up led #0 of ip core
-      uint32_t dvsr = 75000000;   // ping period of 1.5s
-      // TODO check that each of the values being written to the registers is correct
-      /**
-       * --  N: integer := 30;
-       * --  SYS_FRQ: integer := 50000000;
-       * --  PING_FRQ: integer := 20
-       */
-      float fc2n = 21.47483; // in Hz
-      int ping_freq = 10; // in Hz
+      //////////// register map//////////
+      /// fccw -> register 0, bits 29 - 0
+      /// dvsr -> register 1, bits 26 - 0
+      /// trig -> register 2, bit 0
+      ///////////////////////////////////
+
+      uint32_t dvsr = 75000000;            ///< ping period of 1.5s
+
+      float fc2n = 21.47483;               ///< f_c / (2^N) = 50e6 / 2^30
+      int ping_freq = 10;                  ///< desired ping frequency in Hz
       uint32_t fccw = ping_freq * fc2n;
-      io_write(blink_base, 0, fccw); // write i/o for reg 0
-      io_write(blink_base, 1, dvsr); // write i/o reg 1
-      io_write(blink_base, 2, sw.read(0));
-//      io_write(blink_base, 2, 1);
-//      sleep_ms(2);
-//      io_write(blink_base, 2, 0);
-//
-//      int trig = sw.read(0);
-//      if (trig){
-//         io_write(blink_base, 2, trig);// set go bit
-//      }
+      io_write(ping_base, 0, fccw);        ///< write i/o for reg 0
+      io_write(ping_base, 1, dvsr);        ///< write i/o reg 1
+      io_write(ping_base, 2, sw.read(0));  ///< send trigger from switch to ping
    }
 }
